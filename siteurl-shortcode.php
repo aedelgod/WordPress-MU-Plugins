c579d9ab8ebfa3e84b457a46390061aee1ed1573
Dillyn Barber <dillyn@caveconsulting.com>
2018-02-17 00:27:21 -0600

    Added Description
    Gets current site URL with a shortcode. Add to /wp-content/mu-plugins/ (create if doesn't exist). Really useful for multisite networks. Lets you use [url] and will get that site's URL to save from copypasta or redundant uploads if you know the path.
    
    Examples:
    [url]/wp-content/uploads/sites/2/2018/02/image.jpg
    [url]/wp-admin/tools.php?page=domainmapping

--- siteurl-shortcode.php
+++ siteurl-shortcode.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Description: Get site url for links 
+ * Description: Gets current site URL with a shortcode. Add to /wp-content/mu-plugins/ (create if doesn't exist)
  * Plugin Name: Get Home URL
  * Author: Dillyn Barber
  * Author URI: https://www.caveconsulting.com
  */
-function url_shortcode() {
+function make_url_shortcode() {
 return get_bloginfo('url');
 }
 add_shortcode('url','url_shortcode');
